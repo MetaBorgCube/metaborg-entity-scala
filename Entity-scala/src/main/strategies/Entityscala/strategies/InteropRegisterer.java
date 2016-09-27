8c8d6a01ef9001ce0b6fa7a28fb4fc133f4fff69
Daco Harkes <dc.harkes@gmail.com>
2016-09-27 15:55:06 +0200

    java external strategy

--- Entity-scala/src/main/strategies/Entityscala/strategies/InteropRegisterer.java
+++ Entity-scala/src/main/strategies/Entityscala/strategies/InteropRegisterer.java
@@ -3,8 +3,11 @@ package Entityscala.strategies;
 import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
 
+import Entityscala.strategies.java_strategy_0_0;
+
 public class InteropRegisterer extends JavaInteropRegisterer {
-    public InteropRegisterer() {
-        super(new Strategy[] { });
-    }
+	public InteropRegisterer() {
+		super(new Strategy[] {
+				java_strategy_0_0.instance, });
+	}
 }
