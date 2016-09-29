bf64f104298a24403cf4aa641b11443b08cb3621
Daco Harkes <dc.harkes@gmail.com>
2016-09-29 22:22:04 +0200

    scala project: stratego dependencies (no repos added yet)

--- Scalaproject/src/main/scala/org/example/App.scala
+++ Scalaproject/src/main/scala/org/example/App.scala
@@ -1,15 +1,29 @@
 package org.example
 
+import org.spoofax.interpreter.terms.IStrategoTerm
+import org.spoofax.interpreter.terms.IStrategoAppl
+import org.strategoxt.lang.Context
+
 /**
  * @author ${user.name}
  */
 object App {
-  
-  def foo(x : Array[String]) = x.foldLeft("")((a,b) => a + b)
-  
-  def main(args : Array[String]) {
-    println( "Hello World!" )
+
+  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
+
+  def main(args: Array[String]) {
+    println("Hello World!")
     println("concat arguments = " + foo(args))
+  }
+
+  def runAsStrategy(context: Context, inputFromEditor: IStrategoTerm): IStrategoAppl = {
+    // output to the Spoofax console is handled via Context.getIOAgent.printError
+    context.getIOAgent.printError("Hello, World! Your input was: ")
+    context.getIOAgent.printError(inputFromEditor.toString)
+    // you return a tuple for the file you want to show in the editor
+    // tuple: (filename as IStrategoString, contents as IStrategoString)
+    // trivial strategy: don't output any files == None()
+    context.getFactory.makeAppl(context.getFactory.makeConstructor("None", 0))
   }
 
 }
