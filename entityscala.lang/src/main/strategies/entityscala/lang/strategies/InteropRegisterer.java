77fc167601bf2daabf8d08f0b36f01544a52aad0
Daco Harkes <dc.harkes@gmail.com>
2016-10-12 23:56:51 +0200

    code cleanup

--- entityscala.lang/src/main/strategies/entityscala/lang/strategies/InteropRegisterer.java
+++ entityscala.lang/src/main/strategies/entityscala/lang/strategies/InteropRegisterer.java
@@ -3,13 +3,10 @@ package entityscala.lang.strategies;
 import org.strategoxt.lang.JavaInteropRegisterer;
 import org.strategoxt.lang.Strategy;
 
-import entityscala.lang.strategies.java_strategy_0_0;
-
 public class InteropRegisterer extends JavaInteropRegisterer {
 	public InteropRegisterer() {
 		super(new Strategy[] {
 				editor_analyze_0_0.instance,
-				editor_resolve_0_0.instance,
-				java_strategy_0_0.instance, });
+				editor_resolve_0_0.instance, });
 	}
 }
--- entityscala.scala/src/main/scala/org/example/App.scala
+++ entityscala.scala/src/main/scala/org/example/App.scala
@@ -13,48 +13,8 @@ import org.spoofax.terms.attachments.OriginAttachment.tryGetOrigin;
  */
 object App {
 
-  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
-
   def main(args: Array[String]) {
     println("Hello World!")
-    println("concat arguments = " + foo(args))
-  }
-
-  def runAsStrategy(context: Context, inputFromEditor: IStrategoTerm): IStrategoTerm = {
-    // output to the Spoofax console is handled via Context.getIOAgent.printError
-    context.getIOAgent.printError("Hello, World! Your input was: ")
-    context.getIOAgent.printError(inputFromEditor.toString)
-    // you return a tuple for the file you want to show in the editor
-    // tuple: (filename as IStrategoString, contents as IStrategoString)
-    // trivial strategy: don't output any files == None()
-    context.getFactory.makeAppl(context.getFactory.makeConstructor("None", 0))
-
-    val scalaVal = STerm.fromStratego(inputFromEditor);
-    context.getIOAgent.printError(scalaVal.toString());
-
-    val ast = inputFromEditor.getAllSubterms()(0);
-    val scalaAst = STerm.fromStratego(ast);
-    context.getIOAgent.printError(scalaAst.toString());
-
-    val origin = ImploderAttachment.get(tryGetOrigin(ast));
-    origin.getLeftToken.getIndex
-
-    context.getIOAgent.printError(origin.toString());
-    context.getIOAgent.printError(origin.getLeftToken.getStartOffset.toString);
-    context.getIOAgent.printError(origin.getLeftToken.getEndOffset.toString);
-    context.getIOAgent.printError(origin.getRightToken.getStartOffset.toString);
-    context.getIOAgent.printError(origin.getRightToken.getEndOffset.toString);
-
-    val origin2 = ImploderAttachment.get(tryGetOrigin(inputFromEditor));
-    context.getIOAgent.printError(if (origin2 == null) "null" else origin2.toString());
-
-    val astOrig = tryGetOrigin(ast);
-    context.getIOAgent.printError(astOrig.toString());
-
-    val strategoVal = STerm.toStratego(scalaVal)(context)
-
-    return strategoVal
-    //    return context.getFactory.makeString("Regards from scala-strategy")
   }
 
   def editorAnalyze(context: Context, strTerm: IStrategoTerm): IStrategoTerm =
